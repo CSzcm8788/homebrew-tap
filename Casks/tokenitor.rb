fbf03efffb9b70afe69aab470a063edd285deee5
CSzcm8788 <muziwen.logo@gmail.com>
2026-07-11 20:10:54 +0800

    tokenitor 1.4.5

--- Casks/tokenitor.rb
+++ Casks/tokenitor.rb
@@ -1,6 +1,6 @@
 cask "tokenitor" do
-  version "1.4.4"
-  sha256 "ed8ba94a93e3df7fa2c09355dd6ea2c737b0527b66acc9444533b206c1fd2c03"
+  version "1.4.5"
+  sha256 "6b844ff99715e1d90a145a7b02cd85f51294ea1a574e7dc0ad4a97d3861f61bf"
 
   url "https://github.com/CSzcm8788/Tokenitor/releases/download/v#{version}/Tokenitor.dmg"
   name "Tokenitor"
